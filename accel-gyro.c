60d133ae840a2f23fc3fa48b58e27ae35949db5c
Julia Cotter <35545731+juliacotter@users.noreply.github.com>
2019-02-05 15:13:14 -0500

    fixing stuff
    typos, changing names to make easier to read, and depending on what the I2C reads, did both conversions

--- accel-gyro.c
+++ accel-gyro.c
@@ -20,43 +20,43 @@ void setup(accel_address=LSM303_ADDRESS_ACCEL, mag_address=LSM303_ADDRESS_MAG){
 
 void read(){
   //for accel, lsb data then msb
-  accel_data_x1 = wiringPiI2CRead(LSM303_REGISTER_ACCEL_OUT_X_L_A);
-  accel_data_x2 = wiringPiI2CRead(LSM303_REGISTER_ACCEL_OUT_X_H_A);
-  accel_data.x = accel_data_x2 * 256 + accel_data_x2;
-  if accel_data.x > 32767{
-    accel_data.x -= 65536;
+  accel_data_xlo = wiringPiI2CRead(LSM303_REGISTER_ACCEL_OUT_X_L_A);
+  accel_data_xhi = wiringPiI2CRead(LSM303_REGISTER_ACCEL_OUT_X_H_A);
+  accel_data.x = accel_data_xhi * 256 + accel_data_xlo; // or (accel_data_xhi << 8) | accel_data_xlo;
+  if accel_data.x > 32767{ // if accel_data.x > 0111111111111111
+    accel_data.x -= 65536; //   accel_data.x -= 10000000000000000
   }
-  accel_data_y1 = wiringPiI2CRead(LSM303_REGISTER_ACCEL_OUT_Y_L_A);
-  accel_data_y1 = wiringPiI2CRead(LSM303_REGISTER_ACCEL_OUT_Y_H_A);
-  accel_data.y = accel_data_y2 * 256 + accel_data_y1;
+  accel_data_ylo = wiringPiI2CRead(LSM303_REGISTER_ACCEL_OUT_Y_L_A);
+  accel_data_yhi = wiringPiI2CRead(LSM303_REGISTER_ACCEL_OUT_Y_H_A);
+  accel_data.y = accel_data_yhi * 256 + accel_data_ylo;
   if accel_data.y > 32767{
     accel_data.y -= 65536;
   }
-  accel_data_z1 = wiringPiI2CRead(LSM303_REGISTER_ACCEL_OUT_Z_L_A);
-  accel_data_z2 = wiringPiI2CRead(LSM303_REGISTER_ACCEL_OUT_Z_H_A);
-  accel_data.z = accel_data_z2 * 256 + accel_data_z1;
+  accel_data_zlo = wiringPiI2CRead(LSM303_REGISTER_ACCEL_OUT_Z_L_A);
+  accel_data_zhi = wiringPiI2CRead(LSM303_REGISTER_ACCEL_OUT_Z_H_A);
+  accel_data.z = accel_data_zhi * 256 + accel_data_zlo;
   if accel_data.z > 32767{
     accel_data.z -= 65536;
   }
   printf("Acceleration in X-Axis: %d\n", accel_data.x);
   printf("Acceleration in Y-Axis: %d\n", accel_data.y);
   printf("Acceleration in Z-Axis: %d\n", accel_data.z);
-  //for accel, msb data then lsb
-  mag_data_x1 = wiringPiI2CRead(LSM303_REGISTER_MAG_OUT_X_H_M);
-  mag_data_x2 = wiringPiI2CRead(LSM303_REGISTER_MAG_OUT_X_L_M);
-  mag_data.x = mag_data_x1 * 256 + mag_data_x2;
-  if mag_data.x > 32767{
-    mag_data.x -= 65536;
+  //for mag, msb data then lsb
+  mag_data_xhi = wiringPiI2CRead(LSM303_REGISTER_MAG_OUT_X_H_M);
+  mag_data_xlo = wiringPiI2CRead(LSM303_REGISTER_MAG_OUT_X_L_M);
+  mag_data.x = mag_data_xlo + mag_data_xhi * 256; // or accel_data_xlo | (accel_data_xhi << 8);
+  if mag_data.x > 32767{ // if accel_data.x > 0111111111111111
+    mag_data.x -= 65536; //   accel_data.x -= 10000000000000000
   }
-  mag_data_y1 = wiringPiI2CRead(LSM303_REGISTER_MAG_OUT_Y_H_M);
-  mag_data_y2 = wiringPiI2CRead(LSM303_REGISTER_MAG_OUT_Y_L_M);
-  mag_data.y = mag_data_y1 * 256 + mag_data_y2;
+  mag_data_yhi = wiringPiI2CRead(LSM303_REGISTER_MAG_OUT_Y_H_M);
+  mag_data_ylo = wiringPiI2CRead(LSM303_REGISTER_MAG_OUT_Y_L_M);
+  mag_data.y = mag_data_ylo + mag_data_yhi * 256;
   if mag_data.y > 32767{
     mag_data.y -= 65536;
   }
-  mag_data_z1 = wiringPiI2CRead(LSM303_REGISTER_MAG_OUT_Z_H_M);
-  mag_data_z2 = wiringPiI2CRead(LSM303_REGISTER_MAG_OUT_Z_L_M);
-  mag_data.z = mag_data_z1 * 256 + mag_data_z2;
+  mag_data_zhi = wiringPiI2CRead(LSM303_REGISTER_MAG_OUT_Z_H_M);
+  mag_data_zlo = wiringPiI2CRead(LSM303_REGISTER_MAG_OUT_Z_L_M);
+  mag_data.z = mag_data_zlo + mag_data_zhi * 256;
   if mag_data.z > 32767{
     mag_data.z -= 65536;
   }
