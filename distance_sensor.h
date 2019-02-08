e1f420403e1ad84ab194d0a88d1b2dee33371c65
Ryan Maresca <35242801+ram82098@users.noreply.github.com>
2019-02-08 14:04:47 -0500

    Update distance_sensor.h

--- distance_sensor.h
+++ distance_sensor.h
@@ -1,1 +1,21 @@
-//distance sensor stuff TODO
+task main()
+{
+  while (true)
+  {
+    //Make sure the value is not an error
+    if(SensorValue[ultrasonic] > 0)
+    {
+      if (SensorValue[ultrasonic] <= 7) {
+        //there is something within the threshold range
+        motor[leftServo] = -50;
+        motor[rightServo] = 50;
+        wait1Msec(800);
+        } else {
+        //nothing in the threshold range
+        motor[leftServo] = 50;
+        motor[rightServo] = 50;
+      }
+    }
+  }
+}
+// Source: http://www.robotc.net/wikiarchive/Tutorials/Arduino_Projects/Mobile_Robotics/VEX/Use_Ultrasonic_Sensor_To_Avoid_Walls
