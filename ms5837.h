374a3b04ae1f666e8f3f5c7158ab7a92864898d0
Julia Cotter <35545731+juliacotter@users.noreply.github.com>
2019-02-09 23:20:54 -0500

    Update ms5837.h

--- ms5837.h
+++ ms5837.h
@@ -91,37 +91,5 @@ struct ms5837_data {
 	C[7]; // calibration values
 }
 
-/**
- * \brief Reset the MS5837 device
- *
- * \return ms5837_status : status of MS5837
- *       - ms5837_status_ok : I2C transfer completed successfully
- *       - ms5837_status_i2c_transfer_error : Problem with i2c transfer
- *       - ms5837_status_no_i2c_acknowledge : I2C did not acknowledge
- */
-enum ms5837_status ms5837_reset(void);
-
-/**
- * \brief Set  ADC resolution.
- *
- * \param[in] ms5837_resolution_osr : Resolution requested
- *
- */
-void ms5837_set_resolution(enum ms5837_resolution_osr );
-
-/**
- * \brief Reads the temperature and pressure ADC value and compute the compensated values.
- *
- * \param[out] float* : Celsius Degree temperature value
- * \param[out] float* : mbar pressure value
- *
- * \return ms5837_status : status of MS5837
- *       - ms5837_status_ok : I2C transfer completed successfully
- *       - ms5837_status_i2c_transfer_error : Problem with i2c transfer
- *       - ms5837_status_no_i2c_acknowledge : I2C did not acknowledge
- *       - ms5837_status_crc_error : CRC check error on on the PROM coefficients
- */
-enum ms5837_status ms5837_read_temperature_and_pressure(float *, float *);
-
 #endif /* MS5837_H_INCLUDED */
   
